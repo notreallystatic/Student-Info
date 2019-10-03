680fe45ace3ffec32c7d22e32faab3bd16c90120
Sachin Kumar <sk92907@gmail.com>
2019-10-03 06:23:43 +0000

    Connected the addStudent, editStudent, submitDocuments route with MongoDB.

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const express	 =	 require('express'),
-	  app        =	 express(),
-	  mongoose	 = 	 require('mongoose'),
-	  bodyParser =   require('body-parser'),
-	  Student 	 =	 require('./models/student'),
-	  Document   =   require('./models/document');
+const express			= require('express'),
+	  app				= express(),
+	  mongoose			= require('mongoose'),
+	  bodyParser		= require('body-parser'),
+	  Student			= require('./models/student'),
+	  Documents			= require('./models/document');
 
 mongoose.connect('mongodb://localhost:27017/Student-info', {useNewUrlParser: true},function(error) {});
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
 // LOGIN ROUTE
 app.get('/login', (req, res) => {
 	res.render('login');
-})
+});
 
 // INDEX ROUTE
 app.get('/', (req, res) => {
 	res.render('index');
-})
+});
 
 // SHOW ROUTE
 app.get('/show', (req, res) => {
 	res.render('findStudent');
-})
+});
 
 app.post('/show', (req, res) => {
 	//Fetch the Student ID and show the coresponding details
@@ -38,23 +36,22 @@ app.post('/show', (req, res) => {
 app.get('/addStudent', (req, res) => {
 	res.render('addStudent');
 })
+
+app.get('/students',function(req,res){
+	Student.find({},function(err, docs){
+		if(err) {
+			cnosole.log(err);
+		} else {
+			console.log(res.json(docs));
+		}
+  });
+});
+
 // Post route to add a new Student to our DB
 app.post('/addStudent', (req, res) => {
- console.log(req.body);
 	let student = new Student();
-	student.RegnNo=req.body.RegnNo;
-	student.AadhaarNo=req.body.AadhaarNo;
-	student.Name=req.body.Name;
-	student.Fname=req.body.Fname;
-	student.AdmissionYear=req.body.AdmissionYear;
-	student.Gender=req.body.Gender;
-	student.Course=req.body.Course;
-	student.AllotmentCategory=req.body.AllotmentCategory;
-	student.AdmissionMode=req.body.AdmissionMode;
-	student.Contact1=req.body.Contact1;
-	student.Contact2=req.body.Contact2;
-	student.Email=req.body.Email;
-	console.log(req.body.Branch);
+	student = req.body;
+	//console.log(req.body.Branch);
 	
 	if (req.body.Course === "BTech"){
 		student.Branch =req.body.Branch[0];
@@ -69,17 +66,56 @@ app.post('/addStudent', (req, res) => {
 	student.save();
 	
 	res.send("Student post route");
-})
+});
 
 
 // Edit Student Route
+// This route will give provide a choice to the user for selecting a student by which he want to Edit any student.
+// The user can edit any student, either by searching an id or selecting any one from the given list.
 app.get('/edit', (req, res) => {
-	
-	Student.findOne({"RegnNo": "121"}, (err, student) => {
+	Student.find({},function(err, data){
+		if (err) {
+			console.log(err);
+		}	else {
+			res.render('edit', {students: data});
+		}
+  });
+});
+
+app.post('/edit', (req, res) => {
+	res.redirect('/edit/' + req.body.id);
+})
+
+app.get('/edit/:id', (req, res) => {
+	const id = req.params.id;
+	Student.findOne({"RegnNo": id}, (err, student) => {
 		if (err) {
 			res.redirect('/');
 		} else {
-			res.render('edit', {student: student});	
+			if (student)
+				res.render('editStudent', {student: student});
+		}
+	});
+});
+
+app.post('/edit/:id', (req, res) => {
+	const id = req.params.id;
+	let student = req.body;
+	if (req.body.Course === "BTech"){
+		student.Branch =req.body.Branch[0];
+	}
+
+	else if (req.body.Course === "MTech"){
+		student.Branch =req.body.Branch[1];
+	}
+	else{
+			student.Branch = req.body.Branch[2];
+	}
+	Student.findByIdAndUpdate(id, student ,(err, student) => {
+		if (err) {
+			res.redirect('/');
+		} else {
+			res.redirect('/edit');
 		}
 	});
 });
@@ -88,18 +124,27 @@ app.get('/edit', (req, res) => {
 // Submit Document Route
 // This route will allow the admin to Submit Documents of a Student, either by searching a Student using his id or by selecting one from the given table.
 app.get('/submitDocument', (req, res) => {
-	res.render('submitDocument');
-})
+	Student.find({},function(err, data){
+		if (err) {
+			console.log(err);
+		}	else {
+			res.render('submitDocument', {students: data});
+		}
+ 	});
+});
 
-app.post('/submitDocument', (req, res) => {
-	console.log(req.body);
-	res.render('submitDocuments');
+app.post('/submitDocument/searchById', (req, res) => {
+	res.redirect('/submitDocument/' + req.body.id);
 })
 
 // This route will take the admin to submit the documents of the selected/searched student.
 app.get('/submitDocument/:id', (req, res) => {
-	res.render('submitDocumentID');
+	res.render('submitDocumentID', {id: req.params.id});
 })
+app.post('/submitDocument/:id', (req, res) => {
+	res.redirect('/');
+})
+
 
 app.listen(3000, () => {	
 	console.log('Server is running...');
